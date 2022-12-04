29a766f0130aecf9e6c221795731f5acb9465943
Keith Hughitt <keith.hughitt@gmail.com>
2022-12-04 08:59:53 -0800

    added rust solution for day 4 part 1

---
+++ 04/rust/aoc04a.rs
@@ -0,0 +1,53 @@
+//
+// AOC 4a
+//
+use std::cmp;
+use std::fs;
+use std::collections::HashSet;
+
+fn main() {
+    let contents = fs::read_to_string("../input")
+        .expect("Unable to find file!");
+
+    let mut counter = 0;
+
+    for line in contents.lines() {
+        // parse section ranges for each elf
+        let mut elves = line.split(',');
+
+        let elf1 = elves.next().unwrap();
+        let elf2 = elves.next().unwrap();
+
+        let e1_strs: Vec<&str> = elf1.split('-').collect();
+        let e2_strs: Vec<&str> = elf2.split('-').collect();
+
+        // convert string bounds to integers
+        let e1_start: u16 = e1_strs[0].parse().expect("not a number!");
+        let e1_end: u16 = e1_strs[1].parse().expect("not a number!");
+        let e2_start: u16 = e2_strs[0].parse().expect("not a number!");
+        let e2_end: u16 = e2_strs[1].parse().expect("not a number!");
+
+        // generate vector representations of ranges
+        let e1_range = (e1_start..e1_end + 1).collect::<Vec<u16>>();
+        let e2_range = (e2_start..e2_end + 1).collect::<Vec<u16>>();
+        
+        // doesn't work..
+        // let e1_set: HashSet<u16> = HashSet::from(e1_range);
+        // let e2_set: HashSet<u16> = HashSet::from(e2_range);
+
+        let e1_set:HashSet<u16> = e1_range.into_iter().collect();
+        let e2_set:HashSet<u16> = e2_range.into_iter().collect();
+
+        let e1_size = e1_set.len();
+        let e2_size = e2_set.len();
+
+        let intersection = e1_set.intersection(&e2_set).collect::<Vec<_>>();
+
+        if intersection.len() == cmp::min(e1_size, e2_size) {
+            counter = counter + 1;
+        }
+    }
+
+    // print final count
+    println!("Total: {}", counter);
+}
